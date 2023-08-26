a7ff9f220b40401af2ac70433dfa78c83131dbfb
Ricardo <ricardo96gf@gmail.com>
2023-08-26 14:27:40 -0400

    Cambio tamaño logo main

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,9 @@ function App() {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
+            className="mx-auto h-20 w-auto"
+            src="https://www.svgrepo.com/show/530405/line-graph.svg"
+            alt="App de Finanzas"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
@@ -97,6 +97,7 @@ function App() {
               Start a 14 day free trial
             </a>
           </p>
+          
         </div>
       </div>
     </>
